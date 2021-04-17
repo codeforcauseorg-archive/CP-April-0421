2d0af3a3f6749e98386580b6fc4905b98dc66495
darecoder <ektamishra1999@gmail.com>
2021-04-17 18:43:21 +0530

    Lecture 1

---
+++ src/org/codeforcause/lecture1/Main.java
@@ -0,0 +1,65 @@
+package org.codeforcause.lecture1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+//        List<String> list = new ArrayList<>();
+//        list.add("Ekta");
+//        list.add("Shubham");
+//        list.add("Kunal");
+//        list.add("Anuj");
+//
+//        System.out.println(list);
+//        Collections.sort(list);
+//        System.out.println(list);
+//
+//        list.remove("Ekta");
+//
+//        System.out.println(list);
+
+//        Stack<String> stack = new Stack<>();
+//        stack.push("Abhi");
+//        stack.push("Rahul");
+//        stack.push("Shivam");
+//
+//        System.out.println(stack);
+//
+//        System.out.println(stack.pop());
+
+//        Queue<String> queue = new LinkedList<>();
+//        queue.add("Atul");
+//        queue.add("Vivek");
+//        queue.add("Raj");
+//
+//        System.out.println(queue.remove());
+
+//        PriorityQueue<String> pq = new PriorityQueue<>(Comparator.reverseOrder());
+//        pq.add("Nihal");
+//        pq.add("Sourav");
+//        pq.add("Ekta");
+//        pq.add("Saloni");
+//        System.out.println(pq.remove());
+
+//        HashSet<String> set = new HashSet<>();
+//        LinkedHashSet<String> set = new LinkedHashSet<>();
+//        TreeSet<String> set = new TreeSet<>();
+//        set.add("Ravi");
+//        set.add("Vivek");
+//        set.add("Mohan");
+//        set.add("Bhanu");
+//
+//        System.out.println(set);
+
+//        Map<Integer, String> map = new HashMap<>();
+//        Map<Integer, String> map = new LinkedHashMap<>();
+//        Map<Integer, String> map = new TreeMap<>();
+//        map.put(23, "Rishabh");
+//        map.put(1, "Shubham");
+//        map.put(35, "Vikas");
+//        map.put(2, "Raahat");
+//
+//        System.out.println(map);
+     }
+}
