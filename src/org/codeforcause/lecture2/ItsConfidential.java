527f82dcf0490bcfd202739eab6f6281cfcfd8f5
darecoder <ektamishra1999@gmail.com>
2021-04-18 20:36:11 +0530

    Lecture 2

---
+++ src/org/codeforcause/lecture2/ItsConfidential.java
@@ -0,0 +1,19 @@
+package org.codeforcause.lecture2;
+
+public class ItsConfidential {
+    public static void main(String[] args) {
+        String str = "abcd";
+        System.out.println(decrypt(str));
+    }
+
+    public static String decrypt(String str) {
+        int mid = (str.length()-1) / 2;
+        if (mid > 0) {
+            return str.charAt(mid)+
+                    decrypt(str.substring(0,mid)) + decrypt(str.substring(mid+1));
+        } else if (mid < 0) {
+            return "";
+        }
+        return str;
+    }
+}
