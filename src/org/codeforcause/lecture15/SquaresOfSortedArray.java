8da4395cfa0def6fbc0aebf442cd5fcb119af6c1
darecoder <ektamishra1999@gmail.com>
2021-05-30 20:42:25 +0530

    lecture 15

---
+++ src/org/codeforcause/lecture15/SquaresOfSortedArray.java
@@ -0,0 +1,26 @@
+package org.codeforcause.lecture15;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(sortedSquares(nums)));
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        for (int ind = n - 1; ind >= 0; ind--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[ind] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[ind] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
